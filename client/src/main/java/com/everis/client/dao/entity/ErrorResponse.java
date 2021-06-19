84add67366650bbabb89a016f9a924386ebf864e
diazflo <bdiazflo@everis.com>
2021-06-19 02:17:20 -0500

    actualizacion de repositorio

--- client/src/main/java/com/everis/client/dao/entity/ErrorResponse.java
+++ client/src/main/java/com/everis/client/dao/entity/ErrorResponse.java
@@ -2,10 +2,12 @@ package com.everis.client.dao.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse extends DataResponse{
     private HttpStatus status;
     private String message;
